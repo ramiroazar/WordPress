36be0c68c14419a69ed5624a38b2a5f26daea303
Ramiro Azar <ramiroazar@gmail.com>
2015-03-07 15:34:22 +1000

    Edited database configuration file

--- wp-config.sql
+++ wp-config.sql
@@ -72,6 +72,8 @@ INSERT INTO `wp_posts` (`ID`, `post_author`, `post_date`, `post_date_gmt`, `post
 
 UPDATE wp_options
     SET option_value = CASE option_name
+        WHEN 'siteurl' THEN 'http://wordpress.dev/wp'
+        WHEN 'home' THEN 'http://wordpress.dev'
         WHEN 'default_comment_status' THEN 'closed'
         WHEN 'permalink_structure' THEN '/%postname%/'
         WHEN 'active_plugins' THEN 'a:5:{i:0;s:13:"cmb2/init.php";i:1;s:36:"contact-form-7/wp-contact-form-7.php";i:2;s:36:"google-sitemap-generator/sitemap.php";i:3;s:47:"really-simple-captcha/really-simple-captcha.php";i:4;s:24:"wordpress-seo/wp-seo.php";}'
@@ -81,7 +83,7 @@ UPDATE wp_options
         WHEN 'show_on_front' THEN 'page'
         WHEN 'page_on_front' THEN 4
     END
-WHERE option_name IN ('default_comment_status','permalink_structure','active_plugins','template','stylesheet','uploads_use_yearmonth_folders','show_on_front','page_on_front')
+WHERE option_name IN ('siteurl','home','default_comment_status','permalink_structure','active_plugins','template','stylesheet','uploads_use_yearmonth_folders','show_on_front','page_on_front')
 
 /*INSERT INTO `wp_options` (`option_id`, `option_name`, `option_value`, `autoload`) VALUES
 (18, 'default_comment_status', 'closed', 'yes'),
