f9adf8d01b56797b30fc821389e86ec3ce6fb7bf
Ramiro Azar <ramiroazar@gmail.com>
2015-03-26 15:53:35 +1000

    Added favicons and touch icons

--- content/themes/_s/header.php
+++ content/themes/_s/header.php
@@ -14,6 +14,21 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+<?php // Custom ?>
+
+<!--[if lt IE 9]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<meta name="format-detection" content="telephone=no">
+
+<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/apple-icon-touch.png">
+<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+<!--[if IE]><link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico"><![endif]-->
+<!-- or, set /favicon.ico for IE10 win -->
+<meta name="msapplication-TileColor" content="transparent">
+<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/win8-tile-icon.png">
+
 <?php wp_head(); ?>
 </head>
 
