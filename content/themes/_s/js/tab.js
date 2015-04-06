5d87dfb2845f105d8927db0d62889b95be217aae
Ramiro Azar <ramiroazar@gmail.com>
2015-04-06 18:39:43 +1000

    Improved comments on tab script

--- content/themes/_s/js/tab.js
+++ content/themes/_s/js/tab.js
@@ -1,22 +1,34 @@
 +function ($) {
 	'use strict';
 
+	// Define every tabgroup in DOM
 	var $tabgroups	= $(".tabgroup");
-	var $collapse	= false;
-		 $collapse	= $tabgroups.data('collapse');
-	var $hover		= false;
-		 $hover		= $tabgroups.data('hover');
 
+	// For each tabgroup
 	$tabgroups.each(function(){
 
-		var $tabpanels  = $(this).find(".tabpanel");
+		// Define collapse setting
+		var $collapse	= false;
+			 $collapse	= $(this).data('collapse');
+		// Define hover setting
+		var $hover		= false;
+			 $hover		= $(this).data('hover');
+		// Define every child tab
 		var $tabs       = $(this).find('a[data-target]');
+		// Define every child tabpanel
+		var $tabpanels  = $(this).find(".tabpanel");
 
+		// When a child tab is clicked
+		// $tabs.mouseenter( function() {
 		$tabs.on( "click", function(e) {
 
-			//e.preventDefault();
+			// e.preventDefault();
 
+			// Define clicked tab
 			var $clicked = $(this);
+
+			// Define clicked tab target
+			var target = $clicked.data('target');
 
 			function _s_tab_disactivate() {
 	         $tabs.attr('aria-selected', false);
@@ -28,23 +40,26 @@
 				$clicked.addClass('active');				
 			}
 
-			// If collapse is enabled
+			// If collapse enabled
 			if ($collapse === true) {
 
 				// If clicked tab is already active
 				if ($clicked.hasClass("active")) {
+					// Disactivate each tab
 		         _s_tab_disactivate();
 				} else {
+					// Disactivate each tab
 		         _s_tab_disactivate();
+					// Activate clicked tab
 		         _s_tab_activate();
 				}
 
 			} else {
+				// Disactivate each tab
 	         _s_tab_disactivate();
+				// Activate clicked tab
 	         _s_tab_activate();
 			}
-
-			var id = $(this).data('target');
 
 			$tabpanels.each(function(index, element){
 
@@ -58,16 +73,23 @@
 					$(element).addClass('active');		
 				}
 
+				// Disactivate each tabpanel
 				_s_tabpanel_disactivate();
 
+				// If collapse enabled
 				if ($collapse === true) {
+					// If clicked tab is already active
 					if ($clicked.hasClass("active")) {
-						if($(element).attr('id') === id) {
+						// If clicked tab target
+						if($(element).attr('id') === target) {
+							// Activate this tabpanel
 		               _s_tabpanel_activate();
 						}
 					}
 				} else {
-					if($(element).attr('id') === id) {
+					// If clicked tab target
+					if($(element).attr('id') === target) {
+						// Activate this tabpanel
 	               _s_tabpanel_activate();
 					}
 				}
