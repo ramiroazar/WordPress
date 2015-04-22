f3facd3f253937a597e5dc08b9245c8b463ddf8a
Ramiro Azar <ramiroazar@gmail.com>
2015-04-22 18:37:30 +1000

    Improved layout styles

--- content/themes/_s/header.php
+++ content/themes/_s/header.php
@@ -37,10 +37,10 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', '_s' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
-		<div>
-			<div class="site-branding" itemscope itemtype="http://schema.org/Organization">
-				<div>
-					<div class="site-brand">
+		<div class="site-branding section-group" itemscope itemtype="http://schema.org/Organization">
+			<div>
+				<div class="site-brand section">
+					<div>
 						<a class="site-logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" itemprop="url">
 							<img src="http://placehold.it/161x100" alt="<?php bloginfo('name'); ?> Logo" itemprop="logo" />
 							<?php /* <img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="<?php bloginfo('name'); ?> Logo" itemprop="logo" /> */ ?>
@@ -60,23 +60,25 @@
 						<?php endif; ?>
 						<p class="site-description" itemprop="description"><?php bloginfo( 'description' ); ?></p>
 					</div>
-					<?php if (_s_contact()) : ?>
-						<div class="site-contact">
+				</div>
+				<?php if (_s_contact()) : ?>
+					<div class="site-contact section">
+						<div>
 							<a href="tel:<?php echo _s_contact(phone) ?>" class="phone" itemprop="telephone">
 								<?php echo _s_contact(phone) ?>
 							</a>
 						</div>
-					<?php endif; ?>
-				</div>
-			</div><!-- .site-branding -->
+					</div>
+				<?php endif; ?>
+			</div>
+		</div><!-- .site-branding -->
 
-			<div id="site-navigation" class="site-navigation" role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
-				<div>
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php _e( 'Primary Menu', '_s' ); ?></button>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-				</div>
-			</div><!-- .main-navigation -->
-		</div>
+		<div id="site-navigation" class="site-navigation section" role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
+			<div>
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php _e( 'Primary Menu', '_s' ); ?></button>
+				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+			</div>
+		</div><!-- .main-navigation -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
