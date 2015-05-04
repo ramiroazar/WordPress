2fdc6def075e4db482ff8eab10e8cc490240e574
Ramiro Azar <ramiroazar@gmail.com>
2015-05-04 17:13:03 +1000

    Deleted get_attachment_id_from_url() function as it is no longer used by ligthbox adaptive images ajax script

--- content/themes/_s/functions.php
+++ content/themes/_s/functions.php
@@ -420,43 +420,6 @@ require get_template_directory() . '/inc/jetpack.php';
 	}
 	add_shortcode("_s_contact", "_s_contact");
 
-/*
- * Get image id from url 
- *
- * @link https://developer.wordpress.org/reference/functions/attachment_url_to_postid/
- * @link https://pippinsplugins.com/retrieve-attachment-id-from-image-url/
- * @link https://philipnewcomer.net/2012/11/get-the-attachment-id-from-an-image-url-in-wordpress/
- */
-
-	function _s_get_attachment_id_from_url( $attachment_url = '' ) {
- 
-		global $wpdb;
-		$attachment_id = false;
-	 
-		// If there is no url, return.
-		if ( '' == $attachment_url )
-			return;
-	 
-		// Get the upload directory paths
-		$upload_dir_paths = wp_upload_dir();
-	 
-		// Make sure the upload path base directory exists in the attachment URL, to verify that we're working with a media library image
-		if ( false !== strpos( $attachment_url, $upload_dir_paths['baseurl'] ) ) {
-	 
-			// If this is the URL of an auto-generated thumbnail, get the URL of the original image
-			$attachment_url = preg_replace( '/-\d+x\d+(?=\.(jpg|jpeg|png|gif)$)/i', '', $attachment_url );
-	 
-			// Remove the upload path base directory from the attachment URL
-			$attachment_url = str_replace( $upload_dir_paths['baseurl'] . '/', '', $attachment_url );
-	 
-			// Finally, run a custom database query to get the attachment ID from the modified attachment URL
-			$attachment_id = $wpdb->get_var( $wpdb->prepare( "SELECT wposts.ID FROM $wpdb->posts wposts, $wpdb->postmeta wpostmeta WHERE wposts.ID = wpostmeta.post_id AND wpostmeta.meta_key = '_wp_attached_file' AND wpostmeta.meta_value = '%s' AND wposts.post_type = 'attachment'", $attachment_url ) );
-	 
-		}
-	 
-		return $attachment_id;
-	}
-
 /**
  * Register a taxonomy.
  *
