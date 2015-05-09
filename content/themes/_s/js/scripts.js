0dbd528bf3cd3d4be009bb370b4e63b8ea516502
Ramiro Azar <ramiroazar@gmail.com>
2015-05-09 14:45:47 +1000

    Narrowed scope of gallery lightbox initiation to anchor links which contain the word uploads in the href attribute

--- content/themes/_s/js/scripts.js
+++ content/themes/_s/js/scripts.js
@@ -29,11 +29,11 @@ jQuery(document).ready(function($) {
 
 	$('.gallery').each(function() { // the containers for all your galleries
 	   $(this).magnificPopup({
-		   delegate: '.gallery-icon a', // child items selector, by clicking on it popup will open
+		   delegate: '.gallery-icon a[href*=uploads]', // child items selector, by clicking on it popup will open
 		   type: 'image',
 		   title: false,
 		   gallery: {
-		     enabled:true,
+		     enabled: true,
 		     navigateByImgClick: true,
 		   }
 	   });
