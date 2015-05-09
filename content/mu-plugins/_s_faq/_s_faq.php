d8d3e04dfd6faa82ecfb76fc522eb21ae75d14c9
Ramiro Azar <ramiroazar@gmail.com>
2015-05-09 13:12:02 +1000

    Added custom taxonomy category_faq to custom post type faq generated by plugin FAQ

--- content/mu-plugins/_s_faq/_s_faq.php
+++ content/mu-plugins/_s_faq/_s_faq.php
@@ -52,6 +52,43 @@ function _s_post_type_faq_init() {
 }
 
 /**
+ * Register a taxonomy.
+ *
+ * @link http://codex.wordpress.org/Function_Reference/register_taxonomy
+ */
+
+add_action( 'init', '_s_taxonomy_faq_init', 0 );
+
+function _s_taxonomy_faq_init() {
+
+	$labels = array(
+		'name'              => _x( 'Categories', 'taxonomy general name' ),
+		'singular_name'     => _x( 'Category', 'taxonomy singular name' ),
+		'search_items'      => __( 'Search Categories' ),
+		'all_items'         => __( 'All Categories' ),
+		'parent_item'       => __( 'Parent Category' ),
+		'parent_item_colon' => __( 'Parent Category:' ),
+		'edit_item'         => __( 'Edit Category' ),
+		'update_item'       => __( 'Update Category' ),
+		'add_new_item'      => __( 'Add New Category' ),
+		'new_item_name'     => __( 'New Category Name' ),
+		'menu_name'         => __( 'Categories' ),
+	);
+
+	$args = array(
+		'hierarchical'      => true,
+		'labels'            => $labels,
+		'show_ui'           => true,
+		'show_admin_column' => true,
+		'query_var'         => true,
+		'rewrite'           => array( 'slug' => 'category_faq' ),
+	);
+
+	register_taxonomy( 'category_faq', array( 'faq' ), $args );
+
+}
+
+/**
  * Register shortcode.
  *
  * @link https://codex.wordpress.org/Shortcode_API
