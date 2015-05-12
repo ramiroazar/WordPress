d675f766ccd45e44f3bc184bcce31934c589231c
Ramiro Azar <ramiroazar@gmail.com>
2015-05-12 10:48:33 +1000

    Improved sidebar template markup

--- content/themes/_s/sidebar.php
+++ content/themes/_s/sidebar.php
@@ -14,6 +14,25 @@ endif
 ?>
 
 <section id="secondary" class="widget-area" role="complementary" itemscope itemtype="http://schema.org/WPSideBar">
+
 	<h2 class="sidebar-title"><?php _e( 'Sidebar', '_s' ); ?></h2>
+
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+
+	<?php if (is_page(get_theme_mod('page_display_contact'))) : ?>
+
+		<aside id="sidebar-contact" class="site-contact section">
+
+			<div>
+
+				<h3 class="section-title"><?php _e( 'Contact Details', '_s' ); ?></h3>
+
+				<?php get_template_part( 'partials/contact' ); ?>
+
+			</div>
+
+		</aside><!-- .site-contact -->
+
+	<?php endif; ?>
+
 </section><!-- #secondary -->
